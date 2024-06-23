3205f50cc946bc99c276fa3f8bc0f776bf2ca45c
vishva goswami <139674987+VishwaGoswami1407@users.noreply.github.com>
2024-06-23 15:32:32 +0530

    commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "vishwa"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+let isLoggedIn = "vishwa"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "vishwa" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof(stringNumber));
+console.log(stringNumber);
+
+// ************************ OPERATIONS ************************
+
+
